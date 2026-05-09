31795f7521c434002a1f32960ac5a9950968ca8f
guhan <guhangopi8@gmail.com>
2026-05-09 23:57:41 +0530

    added scroll to top component if we click any links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import AlertFeed from './components/AlertFeed';
@@ -12,9 +12,20 @@ import GlobalNotification from './components/GlobalNotification';
 import Footer from './components/Footer';
 import BottomNav from './components/BottomNav';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen bg-gray-50 flex flex-col pb-16 md:pb-0">
         <Navbar />
         <GlobalNotification />
